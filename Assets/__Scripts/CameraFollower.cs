428b9d53aa31c1a1a5c12c2be6c6babd0e65056b
Joel Davis <joeld42@gmail.com>
2019-04-26 23:02:41 -0700

    Add scenes and scripts oops

---
+++ Assets/__Scripts/CameraFollower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform target;
+    Camera camera;
+
+    [Tooltip("Follow margin as % of screen size"), Range(0f, 1f)]
+    public float margin = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = camera.WorldToScreenPoint(target.position);
+        float marginPx = Screen.width * margin;
+        float screenEdge = Screen.width - marginPx;
+        float moveAmt = screenPos.x - screenEdge;
+        if (moveAmt > 0f)
+        {
+            moveAmt = Mathf.Clamp(moveAmt, 0.0f, marginPx);
+            camera.transform.position += Vector3.right * Time.deltaTime * moveAmt;
+        }
+
+    }
+}
---
+++ Assets/__Scripts/SceneryLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneryLoader : MonoBehaviour
+{
+
+    // TODO: make this pick levels more smarter
+    public string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentLevel = SceneManager.GetSceneByName(levelName);
+        if (currentLevel == null)
+        {           
+            SceneManager.LoadScene(levelName, LoadSceneMode.Additive);
+        } else
+        {
+            Debug.LogFormat("Scene {0} already loaded", levelName);
+        }
+    }
+
+}
