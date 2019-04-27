f7d2ecbf28b27123f52f69c20d85ad0a3af29f46
Mach Kobayashi <machwerx@gmail.com>
2019-04-27 14:52:40 -0700

    starting to add hooks for articulation

---
+++ Assets/__Scripts/PigAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigAnimation : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Snout;
+    [SerializeField] private GameObject m_LeftWing;
+    [SerializeField] private GameObject m_RightWing;
+    [SerializeField] private GameObject m_FrontLeftLeg;
+    [SerializeField] private GameObject m_FrontRightLeg;
+    [SerializeField] private GameObject m_BackLeftLeg;
+    [SerializeField] private GameObject m_BackRightLeg;
+    [SerializeField] private GameObject m_Tail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
