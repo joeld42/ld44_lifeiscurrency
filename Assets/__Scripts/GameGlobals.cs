940c52f38d1e7c0932f3cd55d4a36a2db54f25b8
Joel Davis <joeld42@gmail.com>
2019-04-27 10:09:11 -0700

    Add GameGlobals and UIController

---
+++ Assets/__Scripts/GameGlobals.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGlobals : MonoBehaviour
+{
+    public static GameGlobals instance;
+
+    public delegate void GameOverAction();
+    public static event GameOverAction OnGameOver;
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            // The gameGlobals already exists
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TriggerGameOver()
+    {
+        if (OnGameOver != null)
+        {
+            OnGameOver();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/__Scripts/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public PigController heroPig;
+
+    [Header("Child Controls")]
+    public RectTransform gameOverPanel;
+    public TextMeshProUGUI coinsCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverPanel.gameObject.SetActive(false);
+        heroPig.CoinCountChanged += UpdateCoinText;
+        UpdateCoinText(heroPig.coinCount);
+
+        GameGlobals.OnGameOver += GameOver;
+    }
+
+    private void OnDestroy()
+    {
+        GameGlobals.OnGameOver -= GameOver;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateCoinText( int coins )
+    {
+        coinsCounter.text = string.Format("Coins: {0}", coins);
+    }
+
+    void GameOver()
+    {
+        gameOverPanel.gameObject.SetActive(true);
+
+    }
+    public void onPlayAgain()
+    {
+        SceneManager.LoadScene("PigGame");
+    }
+}
