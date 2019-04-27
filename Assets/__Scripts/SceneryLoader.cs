9c13d7ec460ec53e766774c97579b8e8f0e88d09
Tim Speltz <>
2019-04-27 12:29:47 -0700

    using "Level*" as naming convention for scene loader

--- Assets/__Scripts/SceneryLoader.cs
+++ Assets/__Scripts/SceneryLoader.cs
@@ -9,17 +9,27 @@ public class SceneryLoader : MonoBehaviour
     // TODO: make this pick levels more smarter
     public string levelName;
 
+   Scene loadedLevel;
+
     // Start is called before the first frame update
     void Start()
     {
-        Scene scene = SceneManager.GetSceneByName(levelName);
-        if (scene.isLoaded)
-        {      
-            Debug.LogFormat("Scene {0} already loaded", levelName);
-            return;
+
+        // is a Level already loaded?
+        for (int n = 0; n < SceneManager.sceneCount; ++n)
+        {
+            Scene scene = SceneManager.GetSceneAt(n);
+            if (scene.name.StartsWith("Level")) 
+            {
+                loadedLevel = scene;
+                Debug.LogFormat("Scene {0} {1} already loaded", loadedLevel.name, loadedLevel.buildIndex) ;
+                if (scene.buildIndex < 0) Debug.LogError(loadedLevel.name+" is not in Build Settings. It will not ship.");
+                return;
+            }
         }
 
         SceneManager.LoadScene(levelName, LoadSceneMode.Additive);
     }
+
 
 }
