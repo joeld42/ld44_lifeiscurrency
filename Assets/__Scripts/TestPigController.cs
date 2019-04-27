4d516b8d0923343a20e132963e0d94c7d4989ad9
Joel Davis <joeld42@gmail.com>
2019-04-26 21:30:10 -0700

    integrate PigController

---
+++ Assets/__Scripts/TestPigController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPigController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.LogFormat("Moveeee");
+            rb.AddForce(Vector2.right);
+        }
+    }
+}
