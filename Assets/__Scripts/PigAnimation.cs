f75e96ab1ea6e21b3b66127b629dfe41f07aa849
Mach Kobayashi <machwerx@gmail.com>
2019-04-27 15:57:48 -0700

    flapping wings!

--- Assets/__Scripts/PigAnimation.cs
+++ Assets/__Scripts/PigAnimation.cs
@@ -13,6 +13,9 @@ public class PigAnimation : MonoBehaviour
     [SerializeField] private GameObject m_BackRightLeg;
     [SerializeField] private GameObject m_Tail;
 
+    private float m_WingFlapTimer;
+    const float m_FlapSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,26 @@ public class PigAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            FlapWing();
+        }
+
+        if (m_WingFlapTimer > 0)
+        {
+            m_WingFlapTimer -= m_FlapSpeed * Time.deltaTime;
+            if (m_WingFlapTimer < 0)
+            {
+                m_WingFlapTimer = 0;
+            }
+            float angle = 60.0f + 60.0f * (0.5f - 0.5f * Mathf.Cos(2.0f * Mathf.PI * m_WingFlapTimer));
+            m_LeftWing.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+            m_RightWing.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+    }
+
+    void FlapWing()
+    {
+        m_WingFlapTimer = 1.0f;
     }
 }
