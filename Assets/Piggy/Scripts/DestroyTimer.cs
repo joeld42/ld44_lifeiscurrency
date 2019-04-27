7c7f34c6957c7f1f1b2f63269d8b9efc342c8b26
Tim Speltz <>
2019-04-26 21:53:39 -0700

    added bullet shooting and timer to destory bullets

---
+++ Assets/Piggy/Scripts/DestroyTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    public float life = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //should be pooled
+        life -= Time.deltaTime;
+        if (life < 0) Destroy(gameObject);
+        
+    }
+}
