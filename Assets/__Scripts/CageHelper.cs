3fc0cc5b7b23a5ff45ff30e31dfa84923cf02f0f
Joel Davis <joeld42@gmail.com>
2019-04-27 14:58:33 -0700

    cagemelon not really working

---
+++ Assets/__Scripts/CageHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageHelper : MonoBehaviour
+{
+
+    public HingeJoint anchor;
+    public LineRenderer ropeLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ropeLine.positionCount = 3;
+        ropeLine.SetPosition(0, anchor.anchor);
+
+        Vector3 middle = (anchor.anchor + transform.position) * 0.5f;
+        middle = middle + UnityEngine.Random.insideUnitSphere;
+        ropeLine.SetPosition(1, middle );
+        ropeLine.SetPosition(2, transform.position);
+    }
+}
