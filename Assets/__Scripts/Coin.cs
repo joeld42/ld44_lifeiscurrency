ce6d88363d0fa18cac3ad49db6fb803547843341
Jeremy Cowles <jeremy.cowles@gmail.com>
2019-04-28 15:04:28 -0700

    Make coins spin in place

--- Assets/__Scripts/Coin.cs
+++ Assets/__Scripts/Coin.cs
@@ -6,14 +6,25 @@ using UnityEngine;
 public class Coin : MonoBehaviour, IChangeScore
 {
 
-    
+    float rotateAmount = 0;
+    Vector3 angles;
+
     void Start()
     {
+        angles = transform.root.localEulerAngles;
+
         Collider2D collider = GetComponent<Collider2D>();
         if ( ! collider.isTrigger)
         {
             Debug.LogError("Forcing Collider to be a trigger", this);
         }
+    }
+
+    private void Update() {
+        rotateAmount += Time.deltaTime; // (Mathf.Sin(Time.timeSinceLevelLoad * 10.0f) - .5f) * 4;
+        var eu = new Vector3();
+        eu.y = angles.y + rotateAmount * 200;
+        transform.localEulerAngles = eu;
     }
 
     public void ChangeScore(PigController pig)
