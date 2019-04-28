f325eadcdbcc4d98d60eacc7a2f638c6c2e81726
Jeremy Cowles <jeremy.cowles@gmail.com>
2019-04-27 22:46:35 -0700

    Bullet calls cage.Break() on trigger

---
+++ Assets/__Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other) {
+        var cage = other.GetComponent<Cage>();
+        if (cage) {
+            cage.Break();
+            Destroy(gameObject);
+        }
+    }
+}
