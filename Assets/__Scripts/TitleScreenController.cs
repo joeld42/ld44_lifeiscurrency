252dd2915060cc613176470c69b81144fee623f3
Joel Davis <joeld42@gmail.com>
2019-04-28 15:17:42 -0700

    Canvas scaling on Game scene and press button to start on title

--- Assets/__Scripts/TitleScreenController.cs
+++ Assets/__Scripts/TitleScreenController.cs
@@ -14,6 +14,10 @@ public class TitleScreenController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            OnPlayPressed();
+        }
         
     }
 
