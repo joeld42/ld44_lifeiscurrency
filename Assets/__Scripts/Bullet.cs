8c9307f58f93bac842699fa9dbacc576bbfe2a9a
Mach Kobayashi <machwerx@gmail.com>
2019-04-29 13:43:10 -0700

    bullets die after a couple seconds or when they hit something

--- Assets/__Scripts/Bullet.cs
+++ Assets/__Scripts/Bullet.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    private float m_StartTime;
+
+    private void Start()
+    {
+        m_StartTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - m_StartTime > 2.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         var cage = other.GetComponent<Cage>();
         if (cage) {
             cage.Break();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var pig = other.gameObject.GetComponent<PigController>();
+        if (!pig)
+        {
             Destroy(gameObject);
         }
     }
