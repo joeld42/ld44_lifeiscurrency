119ec184e19c77be9e50848931ffea800bc1de91
Mach Kobayashi <machwerx@gmail.com>
2019-04-29 08:27:58 -0700

    added delay to camera move at start

--- Assets/__Scripts/CameraFollower.cs
+++ Assets/__Scripts/CameraFollower.cs
@@ -11,35 +11,45 @@ public class CameraFollower : MonoBehaviour
     public float margin = 0.25f;
 
     private bool m_RestartingGame;
+    private float m_StartTime;
     
     // Start is called before the first frame update
     void Start()
     {
         camera = GetComponent<Camera>();
         GameGlobals.OnGameRestart += GameRestart;
+        m_StartTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 screenPos = camera.WorldToScreenPoint(target.position);
-
-        if (screenPos.x < 0f && !GameGlobals.instance.isGameOver && !m_RestartingGame)
-        {
-            // Flew off the left
-            GameGlobals.instance.TriggerGameOver();
-        }
-
-        float marginPx = Screen.width * margin;
-        float screenEdge = Screen.width - marginPx;
-        float moveAmt = screenPos.x - screenEdge;
-        float minSpeed = GameGlobals.instance.isGameOver ? 0.0f : 2.0f;
-        moveAmt = Mathf.Clamp(moveAmt, minSpeed, marginPx);
-        camera.transform.position += Vector3.right * Time.deltaTime * moveAmt;
-
         if (m_RestartingGame)
         {
+            // delay one frame to give everyone else a chance to update
             m_RestartingGame = false;
+            m_StartTime = Time.time;
+        }
+        else
+        {
+            Vector3 screenPos = camera.WorldToScreenPoint(target.position);
+
+            if (screenPos.x < 0f && !GameGlobals.instance.isGameOver)
+            {
+                // Flew off the left
+                GameGlobals.instance.TriggerGameOver();
+            }
+
+            float marginPx = Screen.width * margin;
+            float screenEdge = Screen.width - marginPx;
+            float moveAmt = screenPos.x - screenEdge;
+
+            // ramp in min speed over the first few seconds of the game
+            float minSpeed = GameGlobals.instance.isGameOver ? 0.0f : 2.0f;
+            minSpeed *= Mathf.SmoothStep(0.0f, 1.0f, (Time.time - m_StartTime) / 3.0f - 1.0f);
+
+            moveAmt = Mathf.Clamp(moveAmt, minSpeed, marginPx);
+            camera.transform.position += Vector3.right * Time.deltaTime * moveAmt;
         }
     }
 
